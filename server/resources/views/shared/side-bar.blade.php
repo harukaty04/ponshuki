1dd935b6fd93feb71d6e3046b30fa4ea055c0d99
harukaty04 <haruka.kimura04@gmail.com>
2021-01-04 09:57:19 +0900

    delete: サイドバー編集のため検索ボタンを削除

--- server/resources/views/shared/side-bar.blade.php
+++ server/resources/views/shared/side-bar.blade.php
@@ -7,13 +7,6 @@
                 
                 <div class="panel-heading">メニュー</div>
                 <div class="panel-body">
-                    <!-- <a href="" class="btn btn-primary btn-block">sesrch</a> -->
-                    {{-- <div class="input-group">
-                        <input type="text" class="form-control" placeholder="テキスト入力欄">
-                        <span class="input-group-btn">
-                            <button type="button" class=" btn btn-primary btn-block">検索</button>
-                        </span>
-                    </div>--}}
                     
                     <a href="" class="btn1 btn btn-primary btn-block ">マイページ</a>
                     <a href="" class="btn2 btn btn-primary btn-block ">
