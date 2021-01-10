aa087eb4bc9082e2d3b51f21e35f32dda7f8c4d6
harukaty04 <haruka.kimura04@gmail.com>
2021-01-10 23:59:10 +0900

    feat: レイアウト編集

---
+++ server/resources/views/menu/profile.blade.php
@@ -0,0 +1,17 @@
+
+
+@extends('layouts.layout')
+
+
+@section('content')
+
+<div class="p-side-origin">
+    <div class="col-sm-4">
+        @include('shared.side-bar')
+    </div>
+    <div class="col-sm-8">
+    profile
+    </div>
+</div>
+
+@endsection
