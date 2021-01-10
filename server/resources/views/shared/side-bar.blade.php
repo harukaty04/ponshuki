a2d59b11c50e83c355cebe7c856daecb101efb20
harukaty04 <haruka.kimura04@gmail.com>
2021-01-11 00:02:27 +0900

    feat: favorite→likesへ変更

--- server/resources/views/shared/side-bar.blade.php
+++ server/resources/views/shared/side-bar.blade.php
@@ -1,23 +1,37 @@
 
-<div>
-    <div class="card mt-3">
-        <div class="card-body">
+<div class="menu mt-3" >
+    <div class="menu-title text-info h4">
 
-            <nav class="panel panel-default">
-                
-                <div class="panel-heading">メニュー</div>
-                <div class="panel-body">
-                    
-                    {{-- <a href="" class="btn1 btn btn-primary btn-block ">マイページ</a> --}}
-                    <a href="/search" class="btn btn-primary btn-block h6">
-                        日本酒を探す <i class="fas fa-search"></i></a>
-                    <a href="/kunshu" class="btn btn-primary btn-block">薫</a>
-                    <a href="/soushu" class="btn btn-primary btn-block">爽</a>
-                    <a href="/junshu" class="btn btn-primary btn-block">醇</a>
-                    <a href="/jukushu" class="btn btn-primary btn-block">熟</a>
-                </div>
-            </nav>
-
+        <div class="p-3 sidebar-hover">
+            <a class="side-bar-link h4" href="/" >
+                <i class="fas fa-home fa-lg pr-3"></i>Home
+            </a>
         </div>
+
+        <div class="p-3 sidebar-hover">
+            <a class="side-bar-link" href="/search" >
+                <i class="fas fa-search fa-lg pr-3"></i>Search
+            </a>
+        </div>
+
+        <div class="p-3 sidebar-hover">
+            <a class="side-bar-link" href="/profile" >
+                <i class="fas fa-user-alt fa-lg pr-3"></i>Profile
+            </a>
+        </div>
+
+        <div class="p-3 sidebar-hover">
+            <a class="side-bar-link" href="/likes" >
+                <i class="far fa-heart fa-lg pr-3"></i>Likes
+            </a>
+        </div>
+
+
+
+
+        {{-- <a href="" class="btn1 btn btn-primary btn-block ">マイページ</a> --}}
+
+        {{-- <a href="/junshu" class="btn btn-primary btn-block">醇</a> --}}
+        {{-- <a href="/jukushu" class="btn btn-primary btn-block">熟</a> --}}
     </div>
 </div>
