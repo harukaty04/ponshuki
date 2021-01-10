daff93b50c21b5cd714659f01e5daa7e0f32c005
harukaty04 <haruka.kimura04@gmail.com>
2021-01-11 00:03:53 +0900

    feat: 評価登録をするためradioボタンの実装

--- server/resources/views/reviews/form.blade.php
+++ server/resources/views/reviews/form.blade.php
@@ -1,28 +1,97 @@
 {{-- 銘柄、香りの評価、味の評価（★）、総合評価（★）、写真登録 --}}
+    @csrf
+    <div class="md-form">
+        <label>日本酒名</label>
+        <input type="text" name="review-title" class="form-control" required value="{{ old('title') }}">
+    </div>
 
-@csrf
-<div class="md-form">
-    <label>日本酒名</label>
-    <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
-</div>
-<div class="nihonshu-image">
-    <label>画像</label>
+    <div class="nihonshu-image pl-5">
+        <label class="nihonshu-image  ">画像</label>
+        <input type="file" name="review-image" accept="image/jpeg, image/png"  class="mx-auto pt-3">
+    </div>
 
-</div>
-<div class="taste-evl">
-    <label>味の濃さ</label>
 
-</div>
-<div class="scent-evl">
-    <label>香りの強さ</label>
+    <div class="taste-ev ">
+        <label class="taste-evl pl-5">味の濃さ</label>
+        <div class="ratebutton text-center">
+            <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g1" value="1">
+                <label for="1">1</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g1" value="2">
+                <label for="2">2</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g1" value="3" checked>
+                <label for="3">3</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g1" value="4">
+                <label for="4">4</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g1" value="5">
+                <label for="5">5</label>
+            </div>
+        </div>
+    </div>
 
-</div>
-<div class="comp-evl">
-    <label>総合評価</label>
 
-</div>
+    <div class="scent-evl">
+        <label class="taste-evl pl-5">香りの強さ</label>
+        <div class="ratebutton text-center">
+                <div class="md-radio md-radio-inline d-inline">
+                <input  type="radio" name="g2" checked>
+                <label for="1">1</label>
+                </div>
+                <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g2" checked>
+                <label for="2">2</label>
+                </div>
+                <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g2" checked>
+                <label for="3">3</label>
+                </div>
+                <div class="md-radio md-radio-inline d-inline">
+                <input type="radio" name="g2">
+                <label for="4">4</label>
+                </div>
+                <div class="md-radio md-radio-inline d-inline">
+                <input  type="radio" name="g2">
+                <label for="5">5</label>
+                </div>
+        </div>
+    </div>
 
-<div class="form-group">
-    <label></label>
-    <textarea name="body" required class="form-control" rows="4" placeholder="メモ">{{ old('body') }}</textarea>
-</div>
+
+    <div class="comp-evl">
+        <label class="taste-evl pl-5">総合評価</label>
+        <div class="ratebutton text-center">
+            <div class="md-radio md-radio-inline d-inline">
+            <input  type="radio" name="g3" checked>
+            <label for="1">1</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+            <input type="radio" name="g3" checked>
+            <label for="2">2</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+            <input  type="radio" name="g3" checked>
+            <label for="3">3</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+            <input type="radio" name="g3">
+            <label for="4">4</label>
+            </div>
+            <div class="md-radio md-radio-inline d-inline">
+            <input  type="radio" name="g3">
+            <label for="5">5</label>
+            </div>
+    </div>
+    </div>
+
+    <div class="form-group">
+        <label></label>
+        <textarea name="body" required class="form-control" rows="4" placeholder="メモ">{{ old('body') }}</textarea>
+    </div>
