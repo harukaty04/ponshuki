ba741b45c47b2c01863a8b9e5a5cab0120771435
harukaty04 <haruka.kimura04@gmail.com>
2021-01-15 00:55:16 +0900

    feat; プロフィールにてユーザーを表示するため編集

---
+++ server/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+
+class UserController extends Controller
+{
+    public function show(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        return view('menu.profile', [
+            'user' => $user,
+        ]);
+    }
+}
