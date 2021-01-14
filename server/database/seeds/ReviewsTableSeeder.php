8d8dc795fa9ed7e36071dcdc4ff821204047e4df
harukaty04 <haruka.kimura04@gmail.com>
2021-01-15 00:48:52 +0900

    feat: 簡単ログイン 機能追加

--- server/database/seeds/ReviewsTableSeeder.php
+++ server/database/seeds/ReviewsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ReviewsTableSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class ReviewsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $user = DB::table('users')->first();
+
+        foreach (range(1, 3) as $num) {
+            DB::table('tasks')->insert([
+                'folder_id' => 1,
+                'title' => "サンプルタスク {$num}",
+                'status' => $num,
+                'due_date' => Carbon::now()->addDay($num),
+                'user_id' => $user->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
