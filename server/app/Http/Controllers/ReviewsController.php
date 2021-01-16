ac8f939ce8d50be6065a4ad6464765054b0754fc
harukaty04 <haruka.kimura04@gmail.com>
2021-01-16 12:11:25 +0900

    feat: Review一括取得する処理を実装

--- server/app/Http/Controllers/ReviewsController.php
+++ server/app/Http/Controllers/ReviewsController.php
@@ -4,28 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Review;
 
 class ReviewsController extends Controller
 {
     /**
-     * MEMO: あとでReview一括取得する処理を実装する
+     * Review一括取得する処理を実装する
      * 
      */
     public function index()
     {
-        return view('top.index');
+        $reviews = Review::all();
+
+        return view('top.index', compact('reviews'));
     }
 
     /**
      * MEMO:データベースにレビュー内容が保存されるようなメソッドを実装する,
      * 画像もpublicに保存できるようにする
     */
+    
     public function create(Request $request)
     {
-        $current_user_id = Auth::user()->id;
-        dd($request->all());
-        // dd($current_user_id);
+        $inputs = $request->all();
+
+        // $inputsが空じゃなかったら実行
+        if ( !empty($inputs) ) {
+            // ログイン中のユーザーIDを取得
+            $current_user_id = Auth::user()->id;
+
+            $value = [
+                'user_id'         => $current_user_id,
+                'title'           => (string) $inputs['title'] ?? '',
+                'content'         => (string) $inputs['content'] ?? '',
+                'taste_intensity' => (int) $inputs['taste_intensity'],
+                'scent_strength'  => (int) $inputs['scent_strength'],
+                'evaluation'      => (int) $inputs['evaluation'],
+                'image'           => (string) $inputs['image'],
+            ];
+            Review::create($value);
+        }
+        return view('top.index');
     }
 }
-
 
