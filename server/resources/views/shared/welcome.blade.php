6ef1705e086b960dc73ff43ec688be01ec979b40
harukaty04 <haruka.kimura04@gmail.com>
2021-01-18 00:04:14 +0900

    delete: レイアウト内の表示を変更したため

--- server/resources/views/shared/welcome.blade.php
+++ server/resources/views/shared/welcome.blade.php
@@ -1,9 +1,5 @@
 <div class="wel-top text-left">
 
-    {{-- <div class="wel-com mb-5 p-side-origin">
-        <div class="page-title h1 ">ponshuki</div>
-        <p >日本酒と初めて出会うあなたのためのアプリ</p>
-    </div> --}}
 
     <div class="p-side-origin">
         <div class="welcome-bg-image pb-5"></div>
