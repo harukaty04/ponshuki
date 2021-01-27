8f9c895b4e2f1176c76468dbc1a0acc280076526
harukaty04 <haruka.kimura04@gmail.com>
2021-01-28 00:10:02 +0900

    feat: sakeAPIメソッド追加

--- server/app/Http/Controllers/SearchController.php
+++ server/app/Http/Controllers/SearchController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function index()
     {
-        return view('user.searchpage');
         //sakeAPI練習
+        $url = "https://muro.sakenowa.com/sakenowa-data/api/brands";
+        $method = "GET";
+
+        //接続
+        $client = new Client();
+
+        $sake_response = $client->request($method, $url);
+
+        $sake_response = $sake_response->getBody();
+        $sake_response = json_decode($sake_response, true);
+        $sake_lists = $sake_response['brands'];
+        // dd($sake_lists);
+        return view('user.searchpage', ['sake_lists' => $sake_lists]);
     }
 }
 
