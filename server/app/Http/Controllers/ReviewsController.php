561bbc32aab39cb4e7d88f7300c9f1e1d3c80f22
harukaty04 <haruka.kimura04@gmail.com>
2021-01-30 16:19:55 +0900

    feat: 投稿ページへのオートコンプリート機能追加

--- server/app/Http/Controllers/ReviewsController.php
+++ server/app/Http/Controllers/ReviewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Review;
+use GuzzleHttp\Client;
 
 class ReviewsController extends Controller
 {
@@ -111,6 +112,28 @@ class ReviewsController extends Controller
 
         }
 
+        public function getSake()
+        {
+            $url = "https://muro.sakenowa.com/sakenowa-data/api/brands";
+            $method = "GET";
+
+            //接続
+            $client = new Client();
     
+            $sake_response = $client->request($method, $url);
+            $sake_response = $sake_response->getBody();
+            $sake_response = json_decode($sake_response, true);
+            $brands = $sake_response['brands'];
+            //ひとつずつ配列を処理する
+            //nameだけを取り出す
+            //配列にいれる
+    
+            foreach ($brands as $brand)
+            {
+                $brand_names[] = $brand['name'];
+            }
+    
+            return response()->json($brand_names);
+        }
 }
 
