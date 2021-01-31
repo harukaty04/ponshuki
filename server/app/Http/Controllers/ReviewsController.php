ae9f1f702201b2eaf431794eed90e8d26614333c
harukaty04 <haruka.kimura04@gmail.com>
2021-01-31 16:23:54 +0900

    add: id,いいね数,JSONのデータについて追記

--- server/app/Http/Controllers/ReviewsController.php
+++ server/app/Http/Controllers/ReviewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Review;
+use App\Like;
 use GuzzleHttp\Client;
 
 class ReviewsController extends Controller
@@ -17,7 +18,11 @@ class ReviewsController extends Controller
      */
     public function index()
     {
-        $reviews = Review::orderBy('created_at', 'desc') // 投稿作成日が新しい順に並べる
+        //withCountのメモ
+        //withCountの中にモデルで定義したリレーション名（'likes')をいれることで、
+        //$review->likes_countが使用できるようになる
+        //likes_countはreview_idに紐づくlikesテーブルのレコードがいくつあるのかを返す
+        $reviews = Review::withCount('likes')->orderBy('created_at', 'desc') // 投稿作成日が新しい順に並べる
             ->get();
 
         $current_user_id = Auth::id();
@@ -31,6 +36,8 @@ class ReviewsController extends Controller
             $current_user_name = Auth::user()->name;
         } else {
             $current_user_name = '';
+
+            
         }
         
 
@@ -135,5 +142,36 @@ class ReviewsController extends Controller
     
             return response()->json($brand_names);
         }
-}
+
+        public function like(Request $request)
+        {
+            $user_id = Auth::user()->id; //1.ログインユーザーのid取得
+            $review_id = $request->review_id; //2.投稿idの取得
+            $already_liked = Like::where('user_id', $user_id)->where('review_id', $review_id)->first(); 
+
+            if (!$already_liked) { //もしこのユーザーがこの投稿にまだいいねしてなかったら
+                $like = new Like; //4.Likeクラスのインスタンスを作成
+                $like->review_id = $review_id; //Likeインスタンスにreview_id,user_idをセット
+                $like->user_id = $user_id;
+                $like->save();
+            } else { //もしこのユーザーがこの投稿に既にいいねしてたらdelete
+                Like::where('review_id', $review_id)->where('user_id', $user_id)->delete();
+            }
+            //5.この投稿の最新の総いいね数を取得
+            $review_likes_count = count(Like::where('review_id', $review_id)->get()) ?? 0;
+
+            // $review_likes_count = Review::withCount('likes')->findOrFail($review_id)->likes_count;
+            // dd($review_likes_count);
+            $param = [
+                'review_likes_count' => $review_likes_count,
+            ];
+            // dd($param);
+            return response()->json($param); //6.JSONデータをjQueryに返 す
+
+            
+        }
+
+
+        
+    }
 
