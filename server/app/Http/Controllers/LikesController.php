7594c38d5fbc477ac912b4977220a07abd1fd395
harukaty04 <haruka.kimura04@gmail.com>
2021-02-01 13:41:41 +0900

    add:u3000ルート名の編集（likes)

--- server/app/Http/Controllers/LikesController.php
+++ server/app/Http/Controllers/LikesController.php
@@ -5,13 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Like;
+use App\Review;
 
 class LikesController extends Controller
 {
     
     public function index()
     {
-        return view('user.likes');
+        
+        // $reviews = Review::withCount('likes')->orderBy('created_at', 'desc') // 投稿作成日が新しい順に並べる
+        //     ->get();
+        $current_user_id = Auth::id();
+        $user_likes_review_ids = Like::where('user_id', Auth::id())->get()->pluck('review_id');
+        $user_likes_reviews = Review::whereIn('id', $user_likes_review_ids)->get();
+        // dd(count($user_likes_reviews));
+        return view('user.likes',[
+            'reviews' => $user_likes_reviews,
+            'current_user_id' => $current_user_id
+            ]);
+        
     }
 
     public function like_review(Request $request)
