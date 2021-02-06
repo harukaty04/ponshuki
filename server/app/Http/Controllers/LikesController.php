106ed393bb7ed9601ec3d48ec6f49ea5b168a48f
harukaty04 <haruka.kimura04@gmail.com>
2021-02-06 22:06:51 +0900

    add: いいねしたレビューのみ一覧表示

--- server/app/Http/Controllers/LikesController.php
+++ server/app/Http/Controllers/LikesController.php
@@ -7,25 +7,25 @@ use Illuminate\Support\Facades\Auth;
 use App\Like;
 use App\Review;
 
+
 class LikesController extends Controller
 {
     
     public function index()
-    {
+    { 
         
-        // $reviews = Review::withCount('likes')->orderBy('created_at', 'desc') // 投稿作成日が新しい順に並べる
-        //     ->get();
         $current_user_id = Auth::id();
-        // $user_likes_review_ids = Like::where('user_id', Auth::id())->get()->pluck('review_id');
-        // $user_likes_reviews = Review::whereIn('id', $user_likes_review_ids)->get();
+        // whereInの第二引数は配列のため、pluckで特定のカラムのみの配列に変換する
+        $user_likes = Like::where('user_id',$current_user_id)
+            ->get()
+            ->pluck('review_id');
 
-        $user_likes_reviews = Review::withCount('likes')->where('user_id', Auth::id())->get();
-
-        // dd(count($user_likes_reviews));
+        $reviews_user_likes = Review::whereIn('id', $user_likes)->get();
 
         return view('user.likes',[
-            'reviews' => $user_likes_reviews,
-            'current_user_id' => $current_user_id
+            'reviews' => $reviews_user_likes,
+            'current_user_id' => $current_user_id,
+
             ]);
         
     }
