81673ac5b04f84dc46c0c825a24ee69a2ec9e026
harukaty04 <haruka.kimura04@gmail.com>
2021-02-10 22:10:59 +0900

    fix: ゲストユーザーの判定を修正

--- server/resources/views/user/edit_profile.blade.php
+++ server/resources/views/user/edit_profile.blade.php
@@ -11,7 +11,7 @@
         <div class ="card mt-4">
             <div class="card-body">
     {{-- フォームの作成 --}}
-            @if (Auth::id() == $current_user_id)
+            @if (Auth::id() == 1)
             <p class="text-danger">
             <b>※ゲストユーザーは、以下を編集できません。</b><br>
             ・アイコン画像<br>
@@ -24,27 +24,29 @@
                 <div class="pb-2 h5 d-inline-block">名前 : </div>
                 
                 
-                @if (Auth::id() == $current_user_id)
-                <div class="pb-2 h5 d-inline-block">ゲストさん </div>
+                @if (Auth::id() == 1)
+            <div class="pb-2 h5 d-inline-block">{{$user}}さん </div>
                 @else
-                <input type="text" name="name" class="form-control w-25 d-inline-block"  value="{{ $user->name ?? old('name') }}" >
+                <input type="text" name="name" class="form-control w-25 d-inline-block"  value="{{ $user ?? old('name') }}" >
                 @endif
 
-                @if (Auth::id() == $current_user_id)
-                <div class="profile-image pt-4 h4">プロフィール画像の変更</div>
+                @if (Auth::id() == 1)
+                <div class="profile-image pt-4 h4">あなたはゲストユーザーです</div>
                 @else
-                <div class="profile-image pt-4 h4">プロフィール画像の変更</div>
+                <div class="profile-image pt-4 h3">プロフィール画像の変更</div>
                 <div>
                 <input type="file" id="image" name="image" accept="image/jpeg, image/png"  class="pt-3 ">
                 <iframe name="form_response" style="display:none;" ></iframe>   
                 </div>
+                <button type="submit" class="update btn blue-gradient float-right wf-mplus1p">変更を保存
+                </button>
+            
                 @endif
 
 
                 {{-- 説明
                 <textarea name="content" required class="form-control wf-mplus1p" rows="4" placeholder="詳細"></textarea> --}}
-                <button type="submit" class="update btn blue-gradient float-right wf-mplus1p">編集する</button>
-            
+                
             </form>
         </div>
     </div>    
