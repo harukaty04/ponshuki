a9ae27fb0bdf9adbf7efd9b1a6e00366045b85a1
harukaty04 <haruka.kimura04@gmail.com>
2021-02-12 00:48:42 +0900

    add: contentの追加

--- server/database/factories/ReviewFactory.php
+++ server/database/factories/ReviewFactory.php
@@ -2,16 +2,18 @@
 
 use Faker\Generator as Faker;
 use App\Models\Review;
+use Illuminate\Support\Str;
 
 // FIXME: 適当に作ったので後で直す！！
 $factory->define(Review::class, function (Faker $faker) {
     return [
         'user_id'         => $faker->randomNumber(),
         'title'           => $faker->title(),
-        'content'         => $faker->title(),
+        'content'         => $faker->text(),
         'taste_intensity' => $faker->randomNumber(),
         'scent_strength'  => $faker->randomNumber(),
         'evaluation'      => $faker->randomNumber(),
         'image'           => $faker->url()
     ];
 });
+
