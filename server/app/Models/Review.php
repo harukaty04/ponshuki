6dd8feb7324ec451d295a27e9306072a368fc766
harukaty04 <haruka.kimura04@gmail.com>
2021-02-13 17:04:49 +0900

    style: タイプヒントの追加

--- server/app/Models/Review.php
+++ server/app/Models/Review.php
@@ -12,10 +12,7 @@ class Review extends Model
 {
     /**
      * モデルと関連しているテーブル
-     *
-     * @var string
      */
-
     protected $table = "reviews";
     // idカラムの更新・挿入はさせない
     protected $guarded = ['id'];
@@ -43,9 +40,11 @@ class Review extends Model
     {
         return $this->hasMany(Like::class);
     }
+    
 
     //後でViewで使う、いいねされているかを判定するメソッド。
-    public function isLikedBy($user): bool 
+    //FIXME: $user_idだけを渡すように変更する
+    public function isLikedBy(User $user): bool 
     {
         return Like::where('user_id', $user->id)->where('review_id', $this->id)->first() !==null;
     }
