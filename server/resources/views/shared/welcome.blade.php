137ad18dab7f8e53000974f4b6473fbb3ccd04bc
harukaty04 <haruka.kimura04@gmail.com>
2021-02-19 09:07:12 +0900

    feat: welcomeページに使い方説明を追記

--- server/resources/views/shared/welcome.blade.php
+++ server/resources/views/shared/welcome.blade.php
@@ -4,30 +4,53 @@
     <div class="p-side-origin">
         <div class="welcome-bg-image mt-5 mb-5"></div>
         <div class="container review_post">
-            <div class="row ">
+            <div class="row">
             <div class="col-sm">
                 <span class="review_title align-content-center h3">日本酒を飲んでレビューを投稿しよう！</span>
                 <div class="welcome-post-image"></div>
             </div>
-            <div class="col-sm ">
+            <div class="col-sm ml-5">
                 <p>簡単！無料登録</p>
-                <div class ="btn center-block">新規登録</div>
+                <div class ="btn btn-primary px-5 center-block">新規登録</div>
                 <p>すでに会員の方はこちら</p>
-                <div class ="btn center-block">ログイン</div>
+                <div class ="btn btn-primary px-5 center-block">ログイン</div>
             </div>
             </div>
         </div>
-
-        <div class="検索機能">
-        <p></p>
+        <div class="container review_post">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="検索機能">
+                        <span class="review_title align-content-center h3">日本酒の銘柄名を入れて検索！</span>
+                    </div>
+                </div>
+                <div class="col-sm">
+                        <img src="/images/searchpage.png" width="300px" height="400px">
+                </div>
         </div>
 
-        <div class="プロフィール">
-
+        <div class="container review_post mt-5">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="プロフィール">
+                        <span class="review_title align-content-center h3">プロフィールを設定して<br>日本酒仲間を探そう！</span>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <img src="/images/profilepage.png" width="300px" height="400px">
+                </div>
         </div>
 
-        <div class="いいね機能">
-
+        <div class="container review_post mt-5">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="いいね機能">
+                        <span class="review_title align-content-center h3">気に入った投稿はいいねして保存</span>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <img src="/images/likespage.png" width="300px" height="400px">
+                </div>
         </div>
     </div>
 </div>
